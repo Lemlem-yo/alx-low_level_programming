3aeef89b6e1f8646790f8fd4caabde27081aedf0
Lemlem-yo <ltsifete@gmail.com>
2022-09-14 13:33:57 +0300

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	_putchar('_');
-       	_putchar('p');
-       	_putchar('u');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-        _putchar('h');
-        _putchar('s');
+	_putchar('h');
+	_putchar('s');
 	_putchar('r');
-        _putchar('\n');
+	_putchar('\n');
 	return (0);
 }

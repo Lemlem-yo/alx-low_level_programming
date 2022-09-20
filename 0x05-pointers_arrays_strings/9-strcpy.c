83c4209403f81a4374c69c0544b41eea35dac2a5
Lemlem-yo <ltsifete@gmail.com>
2022-09-20 16:26:28 +0300

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <string.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strcpy - copy the string
+ * @dest: destination string
+ * @src: the source string wichin is to be copied
+ * Return: a copied cahracter
  */
-int main(void)
+char *_strcpy(char *dest, char *src)
 {
-    char s1[98];
-    char *ptr;
-
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-        return (0);
+	strcpy(dest, src);
+	return (dest);
 }

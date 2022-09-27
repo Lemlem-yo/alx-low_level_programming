3b5dc1ef18e131ae6f6fe731d72d4c1f834ad821
Lemlem-yo <ltsifete@gmail.com>
2022-09-27 12:55:12 +0300

    Task 2'

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -9,6 +9,7 @@
 int main(void)
 {
     char *s = "hello";
+
     char *f;
 
     f = _strchr(s, 'l');
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strchr - predefined function used for finding the occurrence of a charac * ter in a string. it is present in Cstring header file.
+ * _strchr - searches for the first occurence of the character c
  * @s: string to be scanned
  * @c: character to be searched in str
- * Return: charcter or 0 of the character is not found 
+ * Return: string 0
  */
 char *_strchr(char *s, char c)
 {

001678da14bea4586cd8083638c3b1f6b51b69da
Lemlem-yo <ltsifete@gmail.com>
2022-10-10 14:49:48 +0300

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * init_dog - function return value of struct
+  * @d: hold the adress of struct dog
+  * @name: parameter
+  * @age: parameter2
+  * @owner: parameter3
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		printf("the struct was NULL");
+	d = malloc(sizeof(struct dog));
+			d->name = name;
+			d->age = age;
+			d->owner = owner;
+}

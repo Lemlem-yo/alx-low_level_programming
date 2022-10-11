5b8c505e13fe78a10c94af10b8f650373a4cb746
Lemlem-yo <ltsifete@gmail.com>
2022-10-11 15:35:14 +0300

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,71 +3,50 @@
 #include "dog.h"
 
 /**
-  * new_dog - create a new dog struct
-  * @name: pointer for name variable
-  * @age: variable 2
-  * @owner: variable 3
-  * Return: NULL, 0,1
+  * new_dog - create new dog function
+  * @name: first parameter
+  * @age: second parameter
+  * @owner: third parameter
+  * Return: Null, pointer
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
-	int name_l = 0, own_l = 0;
+	dog_t *new;
+	int i, j;
 
-	if (name != NULL && owner != NULL)
-	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
-		my_dog = malloc(sizeof(dog_t));
-	}
-	if (my_dog == NULL)
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
 		return (NULL);
-	my_dog->name = malloc(sizeof(char) * name_l);
-	if (my_dog->name == NULL)
-	{
-		free(my_dog);
-		return (NULL);
-	}
-	my_dog->owner = malloc(sizeof(char) * own_l);
+	for (i = 0; name[i]; i++)
+	{}
+	new->name = malloc((i + 1) * sizeof(char));
 
-	if (my_dog->owner == NULL)
+	for (j = 0; owner[i]; j++)
+	{}
+	new->owner = malloc((j + 1) * sizeof(char));
+
+	if (new->name == NULL || new->owner == NULL)
 	{
-		free(my_dog->name);
-		free(my_dog);
+		free(new->name);
+		free(new->owner);
+		free(new);
 		return (NULL);
 	}
-	my_dog->name = _strcpy(my_dog->name);
-	my_dog->owner = _strcpy(my_dog->owner, owner);
-	my_dog->age = age;
-	return (my_dog);
-}
-/**
- * _strlen - count length of a string
- * @s: string to be counted
- * Return: string length
- */
-int _strlen(char *s)
-{
-int c = 0;
-for (; *s != '\0'; s++)
-{
-c++;
-}
-return (c);
-}
-/**
- * _strcpy - copies a string
- * @dest: where the copied valeu goes
- * @src: where the copied valeu comes
- * Return: dest and copied value
- */
-char *_strcpy(char *dest, char *src)
-{
-int i;
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-dest[i++] = '\0';
-return (dest);
+	for (i = 0; name[i]; i++)
+	{
+		new->name[i] = name[i];
+
+	}
+	new->name = '\0';
+
+	for (j = 0; owner[j]; j++)
+	{
+		new->owner[j] = owner[j];
+	}
+	new->owner = '\0';
+	new->age = age;
+	return (new);
+
+
+
 }

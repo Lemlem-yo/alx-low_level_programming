c7c1b0d0e9c1ad558a762d3ae828e888422cb78b
Lemlem-yo <ltsifete@gmail.com>
2022-10-12 20:19:32 +0300

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+  * print_name - function that print name
+  * @name: a pointer
+  * @f: pointer of function
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		_putchar("IT IS NULL");
+
+	f(name);
+}

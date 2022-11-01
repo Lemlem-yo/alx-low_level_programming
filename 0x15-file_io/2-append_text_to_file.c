d3be7a7f14f51d7657d621da3832e308e1d5ede9
Lemlem-yo <ltsifete@gmail.com>
2022-11-01 22:34:14 +0300

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	while (text_content[count] == '\0')
+	while (text_content[count] != '\0')
 		count++;
 	m = write(fd, text_content, count);
 

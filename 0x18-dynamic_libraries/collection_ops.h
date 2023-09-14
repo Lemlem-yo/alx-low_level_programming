438b49bac10404444f7d44d2268da933f3761296
Lemlem-yo <ltsifete@gmail.com>
2023-09-14 18:27:11 +0300

    taskss

--- 0x18-dynamic_libraries/collection_ops.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLLECTION_OPS_H
-#define COLLECTION_OPS_H
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
-int mod(int a, int b);
-int div(int a, int b);
-#endif

7f0e7d3a9b315224f7ba9cf7b71771dca33d6b7a
Lemlem-yo <ltsifete@gmail.com>
2023-09-15 22:39:18 +0300

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,63 +1,46 @@
 #include "lists.h"
 
 /**
- *  delete_dnodeint_at_index - delete node at give index
- *	@head:list
- * @index:given index
- * Return: -1 or 0
+ * delete_dnodeint_at_index - Delete a node at a given index in a doubly-linked list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @index: The index of the node to be deleted.
+ * Return: 1 if deletion succeeded, -1 if it failed.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *start;
-	unsigned int i;
-	unsigned int len = len_node(head);
+    if (*head == NULL) {
+        return -1; // Empty list, deletion is not possible.
+    }
 
-	start = *head;
-	if (*head == NULL)
-		return (-1);
+    if (index == 0) {
+        // If index is 0, delete the first node and update the head pointer.
+        dlistint_t *temp = *head;
+        *head = (*head)->next;
+        if (*head != NULL) {
+            (*head)->prev = NULL;
+        }
+        free(temp);
+        return 1; // Deletion succeeded.
+    }
 
-	if (index == 0)
-	{
-		start = start->next;
-		free(*head);
-		*head = start;
-		if (start != NULL)
-			start->prev = NULL;
-		return (1);
-	}
-	for (i = 0; i <= index - 1; i++)
-	{
-		start = start->next;
-		if (!start)
-			return (-1);
-	}
-	if (len - 1 == index)
-	{
-		start->prev->next = NULL;
-		free(start);
-		return (1);
-	}
-	start->prev->next = start->next;
-	start->next->prev = start->prev;
-	free(start);
-	return (1);
-}
-/**
- * len_node - list len
- *
- * @node:list
- * Return:unsigned int
- */
-unsigned int len_node(dlistint_t **node)
-{
-	unsigned int len = 0;
-	dlistint_t *start;
+    unsigned int count = 0;
+    dlistint_t *current = *head;
 
-	start = *node;
-	while (start != NULL)
-	{
-		len += 1;
-		start = start->next;
-	}
-	return (len);
+    while (current != NULL) {
+        if (count == index) {
+            // Update the prev and next pointers of adjacent nodes.
+            if (current->prev != NULL) {
+                current->prev->next = current->next;
+            }
+            if (current->next != NULL) {
+                current->next->prev = current->prev;
+            }
+            free(current);
+            return 1; // Deletion succeeded.
+        }
+        current = current->next;
+        count++;
+    }
+
+    return -1; // Index is out of range.
 }

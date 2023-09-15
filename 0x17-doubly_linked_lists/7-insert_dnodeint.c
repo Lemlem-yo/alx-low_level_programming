92e80c047f505cf8eac9d927058fb265695f8e7a
Lemlem-yo <ltsifete@gmail.com>
2023-09-15 22:30:28 +0300

    task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function add node to specific location
+ * @head: pointer to the node
+ * @idx: integer for the index
+ * @n: integer for the node data
+ * Return: node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int countIndex = 0;
+	dlistint_t *new_node = NULL;
+	dlistint_t *current = *h;
+
+	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	while (current != NULL)
+	{
+		if (countIndex == idx - 1)
+		{
+			new_node->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = new_node;
+			current->next = new_node;
+			new_node->prev = current;
+			return (new_node);
+		}
+		current = current->next;
+		countIndex++;
+	}
+
+	if (countIndex == idx)
+	{
+		current->next = new_node;
+		new_node->prev = current;
+		return (new_node);
+	}
+
+	free(new_node);
+	return (NULL);
+}
